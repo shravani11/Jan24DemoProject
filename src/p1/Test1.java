6a97e97ac92eeed0b067a5180f0e6e75c0dd3809
Shravani Rapelli <shravanirapelli11.com>
2024-04-06 11:12:16 +0530

    Resolve Conflicts

--- src/p1/Test1.java
+++ src/p1/Test1.java
@@ -5,5 +5,6 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("In Test1 file");
 		System.out.println("Line number 6 change in Test1");
+		System.out.println("Branch B Line 7 change");
 	}
 }
